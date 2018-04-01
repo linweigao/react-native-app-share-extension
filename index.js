fa5b024eb4024efad2a84bfe8a3720bf881facda
Gao <jogao1983@gmail.com>
2018-04-01 00:22:59 -0700

    MobileSharingEx

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ import { AppRegistry } from 'react-native';
 import App from './App';
 
 AppRegistry.registerComponent('MobileSharing', () => App);
+AppRegistry.registerComponent('MobileSharingEx', () => App);
---
+++ ios/MobileSharingEx/MobileSharingEx.m
@@ -0,0 +1,40 @@
+//
+//  MobileSharingEx.m
+//  MobileSharingEx
+//
+//  Created by Gao, Linwei on 3/31/18.
+//  Copyright © 2018 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "ReactNativeShareExtension.h"
+#import <React/RCTBundleURLProvider.h>
+#import <React/RCTRootView.h>
+#import <React/RCTLog.h>
+
+@interface MobileSharingEx : ReactNativeShareExtension
+@end
+
+@implementation MobileSharingEx
+
+RCT_EXPORT_MODULE();
+
+- (UIView*) shareView {
+  NSURL *jsCodeLocation;
+  
+  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+  
+  RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
+                                                      moduleName:@"MobileSharingEx"
+                                               initialProperties:nil
+                                                   launchOptions:nil];
+  rootView.backgroundColor = nil;
+  
+  // Uncomment for console output in Xcode console for release mode on device:
+  // RCTSetLogThreshold(RCTLogLevelInfo - 1);
+  
+  return rootView;
+}
+
+@end
+
